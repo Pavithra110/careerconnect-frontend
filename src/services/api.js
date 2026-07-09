3b69329f984f35e5c94e9fdc051e46db298829f4
Pavithra110 <pavithradhana2020@gmail.com>
2026-07-09 09:08:41 +0530

    Updated Railway backend URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://careerconnect-backend-production-7399.up.railway.app/api"
 });
 
 export default api;
